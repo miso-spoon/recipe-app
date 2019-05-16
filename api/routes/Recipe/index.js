5e81b2b66e98a6845bc957d508796c34bccae45a
Steffan <skbrown333@gmail.com>
2019-05-16 15:24:25 -0500

    chore(recipe): comment routes

--- api/routes/Recipe/index.js
+++ api/routes/Recipe/index.js
@@ -5,11 +5,15 @@ const logRoutes = require('../../utils/logging').logRoutes;
 
 let Controller = new RecipeController();
 
+/* GET */
 routes.get('/', wrapAsync(Controller.getAll));
 routes.get('/:id', wrapAsync(Controller.getById));
+/* PUT */
 routes.put('/:id', wrapAsync(Controller.update));
-routes.delete('/:id', wrapAsync(Controller.remove));
+/* POST */
 routes.post('/create', wrapAsync(Controller.create));
+/* DELETE */
+routes.delete('/:id', wrapAsync(Controller.remove));
 
 logRoutes('/recipes', routes);
 
